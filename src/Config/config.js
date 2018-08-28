3908f979f02b9771c7052801139a02f40e6a6c1d
John Doe <ajflory9@yahoo.com>
2018-08-28 13:19:37 -0400

    set up firebase database

---
+++ src/Config/config.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  export const  DB_CONFIG = {
+    apiKey: "AIzaSyAwGWwhLqbfqMBQoe1ySuPXEzRTOAwn4LM",
+    authDomain: "react-notes-85766.firebaseapp.com",
+    databaseURL: "https://react-notes-85766.firebaseio.com",
+    projectId: "react-notes-85766",
+    storageBucket: "react-notes-85766.appspot.com",
+    messagingSenderId: "986327594739"
+  };
+ 
